1fb9d1026aa85b508fd7f3b9a816593c75e0f665
Brayan Monroy <brayansneider22@gmail.com>
2025-11-14 21:58:59 -0500

    update

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 import deepinv as dinv
 import tqdm
 import os
@@ -8,7 +9,6 @@ from models import ModuloSEFLnet
 
 import matplotlib.pyplot as plt
 
-DATA_ROOT = os.path.join(".", "data", "unmodnet")
 MAX_VALUE = 4.0
 THRESHOLD = 1.0
 MODE = "floor"
@@ -17,33 +17,41 @@ epochs = 100
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-_, test_dataset = load_dataset(DATA_ROOT, max_val=MAX_VALUE)
-
 
 physics = dinv.physics.SpatialUnwrapping(threshold=THRESHOLD, mode=MODE).to(device)
-model =  ModuloSEFLnet(mx=THRESHOLD, in_channels=n_channels, out_channels=n_channels).to(device)
-model.load_state_dict(torch.load(os.path.join("ckpts", "ModuloSEFLnet.pth")))
-# print numeber of parameters in K
-num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-print(f"Number of trainable parameters in the model: {num_params/1e3:.2f} K")
+# model =  ModuloSEFLnet(mx=THRESHOLD, in_channels=n_channels, out_channels=n_channels).to(device)
+# model.load_state_dict(torch.load(os.path.join("ckpts", "ModuloSEFLnet.pth")))
+# # print numeber of parameters in K
+# num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+# print(f"Number of trainable parameters in the model: {num_params/1e3:.2f} K")
 
 
 psnr_fn = dinv.loss.metric.PSNR(max_pixel=MAX_VALUE)
 ssim_fn = dinv.loss.metric.SSIM(max_pixel=MAX_VALUE)
 
-model.eval()
+# model.eval()
 
-sample =  test_dataset[0]
-img, _ = sample
+
+data_path = os.path.join("data", "unmodnet_test", "source")
+img_path = os.listdir(data_path)[93]
+print(img_path)
+
+img = np.load(os.path.join(data_path, img_path))
+img = torch.from_numpy(img).float().permute(2,0,1)  # HWC to CHW
 img = img.unsqueeze(0).to(device)
+img = img.clamp(min=0.0) * MAX_VALUE / img.max()
+
 y   = physics(img)
 
-with torch.no_grad():
-    x_rec = model(y)
-    psnr = psnr_fn(x_rec, img).item()
-    ssim = ssim_fn(x_rec, img).item()
-    print(f"PSNR: {psnr:.2f} dB, SSIM: {ssim:.4f}")
+# with torch.no_grad():
+#     x_rec = model(y)
+#     psnr = psnr_fn(x_rec, img).item()
+#     ssim = ssim_fn(x_rec, img).item()
+#     print(f"PSNR: {psnr:.2f} dB, SSIM: {ssim:.4f}")
 
+psnr = 0
+ssim = 0
+x_rec = img.clone() # 
 # plot results
 
 x_rec /= MAX_VALUE
